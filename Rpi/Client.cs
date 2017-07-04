35c15e90b37fac6ef369ced5ad580b9ea497a76f
Maxim Suntsev <masuntsev@edu.hse.ru>
2017-07-05 00:31:45 +0500

    Создан класс клиента.

---
+++ Rpi/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    /// <summary>
+    /// Класс клиента.
+    /// </summary>
+    public class Client
+    {
+        /// <summary>
+        /// Конструктор по умолчанию.
+        /// </summary>
+        public Client()
+        {
+
+        }
+    }
+}
